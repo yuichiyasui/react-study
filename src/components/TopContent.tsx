8d1e040982d5628641953bf026704926fcdb74e3
yuichiyasui <yassan.b.a.w@gmail.com>
2020-07-02 01:56:29 +0900

    ルーティングを設定

--- src/components/TopContent.tsx
+++ src/components/TopContent.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper'
 import IconButton from '@material-ui/core/IconButton'
 import SearchIcon from '@material-ui/icons/Search'
@@ -7,13 +8,19 @@ import InputBase from '@material-ui/core/InputBase'
 import 'assets/css/components/topContent.scss'
 
 const TopContent: FC = () => {
+  /** State */
   const [keyword, setKeyword] = useState('')
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(keyword) // TODO: ESListの警告回避のためにとりあえず入れた
     setKeyword(event.target.value)
   }
+
+  /** ルーティング */
+  const history = useHistory()
+  const handleSubmit = () => {
+    history.push('/search/' + keyword)
+  }
   return (
-    <Paper className="paper">
+    <Paper className="paper" component="form" onSubmit={handleSubmit}>
       <IconButton>
         <SearchIcon type="submit" />
       </IconButton>
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,12 +1,24 @@
 import React, { FC } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import TopContent from 'components/TopContent'
+
 import 'assets/css/pages/top.scss'
 
 const Top: FC = () => {
   return (
-    <div className="bg-img">
-      <TopContent />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="bg-img">
+            <TopContent />
+          </div>
+        </Route>
+        <Route path="/search/:keyword" exact>
+          検索結果
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 export default Top
